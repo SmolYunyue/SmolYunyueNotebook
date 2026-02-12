20a01075243882f26511834833832cfe3d877692
SmolYunyue <zhantaoli2435@gmail.com>
2026-02-11 23:24:30 -0700

    uwu

---
+++ CS/CS_python/data_type.py
@@ -0,0 +1,10 @@
+# print the identity, type, and the value for 4
+print(id(4),type(4),4)
+# type of any type is a type, the type itself is a type
+print(type(type(4)))
+print(type(type(type(4))))
+
+# an example for the above data types
+print(True,1,1.0,1+0j)
+
+print(complex(1.0))
